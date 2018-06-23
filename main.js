37e5dce4869c0b319a698bf6eaec2d57c90d17d2
Michael Lesniak <mlesniak@micromata.de>
2018-06-23 07:19:39 +0200

    Display a char

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
-function setup() {
+class Char {
+    constructor(x, y, speed) {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.setRandomCharacter();
+    }
 
+    setRandomCharacter() {
+        let min = 65;
+        let max = 65 + 26;
+        this.char = String.fromCharCode(random(min, max));
+    }
+
+    render() {
+        fill(0, 200, 0);
+        text(this.char, this.x, this.y)
+    }
+}
+
+let char;
+
+function setup() {
+    let cnv = createCanvas(windowWidth + 8, windowHeight + 8);
+    char = new Char(100, 100, 0);
 }
 
 function draw() {
+    background(0);
+    char.render();
 }
